3a6a9bfc7993e63033cdfcb7306c6b5209d982ab
Austin Green <austinzgreen@gmail.com>
2022-07-17 17:03:24 -0400

    fix: safari rendering bug of logo

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,12 +5,12 @@ import Paragraph from 'components/Paragraph'
 import Signup from 'components/Signup'
 
 const Home: NextPage = () => (
-  <div className="bg-black text-white text-center flex flex-col min-h-screen items-center justify-center space-y-20">
+  <div className="bg-black text-white flex flex-col min-h-screen items-center justify-center space-y-20">
     <div className="uppercase">
       <Title level={1} className="text-malachite-green">
         Pronouns
       </Title>
-      <Paragraph>A Nouns Experience for Power Users</Paragraph>
+      <Paragraph className="text-center">A Nouns Experience for Power Users</Paragraph>
     </div>
     <Signup />
     <Marquee text="A Llama X Vector Collaboration" />
