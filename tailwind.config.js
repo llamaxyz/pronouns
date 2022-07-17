6e66527133f945c09b0ba618d2827e04ad920d99
Austin Green <austinzgreen@gmail.com>
2022-07-17 14:34:32 -0400

    fix: invert the direction of the marquee

--- tailwind.config.js
+++ tailwind.config.js
@@ -26,12 +26,12 @@ module.exports = {
       },
       keyframes: {
         marquee: {
-          '0%': { transform: 'translateX(-100%)' },
-          '100%': { transform: 'translateX(0%)' },
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-100%)' },
         },
         marquee2: {
-          '0%': { transform: 'translateX(0%)' },
-          '100%': { transform: 'translateX(100%)' },
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(0%)' },
         },
       },
     },
