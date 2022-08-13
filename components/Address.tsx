2aa8f780e99b5bd16bd0d2804251e82b884060cb
Austin Green <austinzgreen@gmail.com>
2022-08-13 02:13:16 -0400

    add search to nav

--- components/Address.tsx
+++ components/Address.tsx
@@ -1,6 +1,7 @@
 import { ethers } from 'ethers'
 import { ExternalLinkIcon } from '@heroicons/react/outline'
 import Paragraph from 'components/Paragraph'
+import Title from 'components/Title'
 import { truncateAddress } from 'utils/index'
 
 interface Bid {
@@ -22,13 +23,48 @@ type ListProps = {
 
 const Header = ({ address, txHash }: HeaderProps) => (
   <div className="bg-white/10 rounded-lg py-2 px-3">
-    <Paragraph className="xxs:text-sm text-xs opacity-60">Highest Bidder</Paragraph>
-    <Paragraph className="flex items-center justify-between">
-      {truncateAddress(address)}
+    <Paragraph className="xxs:text-sm text-xs opacity-60 mb-2">Highest Bidder</Paragraph>
+    <Paragraph className="flex items-center justify-between mb-4">
+      <span className="flex items-center">
+        <img className="h-6 w-6 mr-2 rounded-full" src={`https://cdn.stamp.fyi/avatar/${address}`} />
+        <span>{truncateAddress(address)}</span>
+      </span>
       <a rel="noopener noreferer noreferrer" target="_blank" href={`https://etherscan.io/tx/${txHash}`}>
         <ExternalLinkIcon className="opacity-60 h-4 w-4" />
       </a>
     </Paragraph>
+    <div className="flex justify-between">
+      <div>
+        <div>
+          <Paragraph className={`xxs:text-sm text-xs opacity-60 mb-1`}>Wallet Balance</Paragraph>
+        </div>
+        <div>
+          <Title level={6} className={`tracking-wide font-normal text-base`}>
+            Ξ 0.‍68
+          </Title>
+        </div>
+      </div>
+      <div>
+        <div>
+          <Paragraph className={`xxs:text-sm text-xs opacity-60 mb-1`}>Bids (Current)</Paragraph>
+        </div>
+        <div>
+          <Title level={6} className={`tracking-wide font-normal text-base`}>
+            1
+          </Title>
+        </div>
+      </div>
+      <div>
+        <div>
+          <Paragraph className={`xxs:text-sm text-xs opacity-60 mb-1`}>Bids (All Time)</Paragraph>
+        </div>
+        <div>
+          <Title level={6} className={`tracking-wide font-normal text-base`}>
+            1
+          </Title>
+        </div>
+      </div>
+    </div>
   </div>
 )
 
@@ -37,7 +73,10 @@ const List = ({ items }: ListProps) => {
     <div className="py-2 px-3 flex flex-col gap-y-4">
       {items?.map((bid: Bid) => (
         <Paragraph key={bid.id} className="flex items-center justify-between opacity-60">
-          {truncateAddress(bid?.bidder?.id)}
+          <span className="flex items-center">
+            <img className="h-6 w-6 mr-2 rounded-full" src={`https://cdn.stamp.fyi/avatar/${bid?.bidder?.id}`} />
+            <span>{truncateAddress(bid?.bidder?.id)}</span>
+          </span>
           <span className="flex items-center gap-x-4">
             <span>Ξ {ethers.utils.formatEther(bid?.amount || 0)}</span>
             <a rel="noopener noreferer noreferrer" target="_blank" href={`https://etherscan.io/tx/${bid?.id}`}>
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,3 +1,4 @@
+import { SearchIcon } from '@heroicons/react/outline'
 import Title from 'components/Title'
 import Connect from 'components/Connect'
 
@@ -7,6 +8,15 @@ const Nav = () => {
       <Title level={5} className="text-malachite-green uppercase" isLogo>
         Pronouns
       </Title>
+      <div className="relative hidden md:block">
+        <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+          <SearchIcon className="w-5 h-5 text-gray-500 dark:text-gray-400" />
+        </div>
+        <input type="search" placeholder="Search" className="block pl-10 w-full text-sm p-2.5 bg-white/10 rounded-xl" />
+        <div className="flex absolute inset-y-0 right-0 items-center pr-3 pointer-events-none">
+          <span className="text-gray-500 dark:text-gray-400 border border-white/10 p-1 text-xs rounded-md">⌘ + K</span>
+        </div>
+      </div>
       <Connect />
     </div>
   )
