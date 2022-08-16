34c54effaec61920913640e8b2ebd74b8993b8c7
Austin Green <austinzgreen@gmail.com>
2022-08-16 00:24:08 -0400

    start error handling

--- components/Address.tsx
+++ components/Address.tsx
@@ -52,23 +52,21 @@ const Header = ({ address, txHash }: HeaderProps) => (
   </div>
 )
 
-const List = ({ items }: ListProps) => {
-  return (
-    <div className="py-2 px-3 flex flex-col gap-y-4 max-h-[32rem] overflow-scroll">
-      {items?.map((bid: Bid) => (
-        <Paragraph key={bid.id} className="flex items-center justify-between opacity-60">
-          <Account alwaysAvatar address={bid?.bidder?.id} />
-          <span className="flex items-center gap-x-4">
-            <span>Ξ {ethers.utils.formatEther(bid?.amount || 0)}</span>
-            <a rel="noopener noreferer noreferrer" target="_blank" href={`https://etherscan.io/tx/${bid?.id}`}>
-              <ExternalLinkIcon className="opacity-60 h-4 w-4" />
-            </a>
-          </span>
-        </Paragraph>
-      ))}
-    </div>
-  )
-}
+const List = ({ items }: ListProps) => (
+  <div className="py-2 px-3 flex flex-col gap-y-4 max-h-[32rem] overflow-scroll">
+    {items?.map((bid: Bid) => (
+      <Paragraph key={bid.id} className="flex items-center justify-between opacity-60">
+        <Account alwaysAvatar address={bid?.bidder?.id} />
+        <span className="flex items-center gap-x-4">
+          <span>Ξ {ethers.utils.formatEther(bid?.amount || 0)}</span>
+          <a rel="noopener noreferer noreferrer" target="_blank" href={`https://etherscan.io/tx/${bid?.id}`}>
+            <ExternalLinkIcon className="opacity-60 h-4 w-4" />
+          </a>
+        </span>
+      </Paragraph>
+    ))}
+  </div>
+)
 
 const pkg = {
   Header,
